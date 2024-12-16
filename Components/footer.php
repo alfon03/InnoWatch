6d218f0b98b70e489917d4b548cf7e5bdda9f9bf
SoyaR08 <rafa140200@gmail.com>
2024-12-16 11:29:31 +0100

    Subido footer en /Components

--- Components/footer.php
+++ Components/footer.php
@@ -1,3 +1,9 @@
-<footer class="bg-marian-blue">
-    <h1>footer</h1>
+<footer class="bg-marian-blue text-light">
+    
+    <div class="interest"></div>
+    <p class="footerMain">InnovaWatch S.A 2024 &copy;</p>
+    <div class="social">
+        <a href=" https://x.com/innovawatch?t=INDYsRH5XOkVjcHnez8QZQ&s=09 "><img src="<?php echo $url . "/../assets/img/twitter-svgrepo-com.svg"?>" alt="twitter" height="20" width="20"></a>
+    </div>
+
 </footer>
